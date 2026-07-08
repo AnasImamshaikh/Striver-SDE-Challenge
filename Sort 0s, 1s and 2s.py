80701af268a2857182d2c818299cb453472513da
Anas Imam Shaikh <anasimamshaikh@eng.rizvi.edu.in>
2026-07-08 23:14:36 +0530

    Implement Dutch National Flag algorithm for sorting

--- Sort 0s, 1s and 2s.py
+++ Sort 0s, 1s and 2s.py
@@ -9,3 +9,22 @@ class Solution:
             else:
                 two.append(arr[i])
         arr[:] = zero + one + two
+
+class Solution:
+    def sort012(self, arr):
+        n= len(arr)
+        l,m =0,0
+        h = n-1
+        
+        while m<=h:
+            if arr[m] == 0:
+                arr[l],arr[m] = arr[m], arr[l]
+                l+=1
+                m+=1
+            elif arr[m] == 1:
+                m+=1
+            else:
+                arr[m],arr[h] = arr[h], arr[m]
+                h-=1
+                
+        return arr 
